c77ae0882c6188ddbe9cef692675ad31ad014e5e
Pratap Neaupane <sereversion@gmail.com>
2020-05-12 18:42:47 +0545

    created Model for Post and register models to admin

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 # from django.http import HttpResponse
-
-posts = [
-	{
-		'author': 'CoreyMS',
-		'title': 'Blog Post 1',
-		'content': 'First post content',
-		'date_posted': 'May 12, 2020',
-	},
-	{
-		'author': 'Jane Doe',
-		'title': 'Blog Post 2',
-		'content': 'Second post content',
-		'date_posted': 'May 13, 2020',
-	}
-
-]
 
 
 # Create your views here.
 def home(request):
 	# return HttpResponse('<h1>Blog Home</h1>')
 	context = {
-		'posts': posts,
+		# Post.objects.all() fetch all data from table Post
+		'posts': Post.objects.all(),
 	}
 	return render(request, 'blog/home.html', context)
 
